a3bdaa3a8b30297b0c6ae7b236b3ec097e378948
Richard Lionggiwijaya <richardLionggiwijaya@hotmail.com>
2021-09-16 14:33:21 +0700

    materi 4

--- coba-laravel/resources/views/about.blade.php
+++ coba-laravel/resources/views/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>about</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
     <h1>halaman about</h1>
-    <h3><?= $name; ?></h3>
-    <h3><?= $email; ?> </h3>
-    <img src="img/<?= $image; ?> " alt="<?= $name; ?> " width= 200px>
-</body>
-</html>
+    <h3>{{ $name }}</h3>
+    <h3>{{ $email }}  </h3>
+    <img src="img/{{ $image }} " alt="{{ $name }} " width= 200px>
+@endsection
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,19 +14,74 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
+        "title" => "About",
         "name" => "Richard",
         "email" => "richardlionggiwijaya828@gmail.com",
         "image" => "download.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+
+    $blog_posts =[
+        [
+            "title" =>"Judul Post",
+            "slug" =>"judul-post-pertama",
+            "author" =>"Nama author Post",
+            "body" =>"dajsfjdas;fjdas;fjdsafjd;safjdsajf;dsjfasj"
+        ],
+        [
+            "title" =>"Judul Post kedua",
+            "slug" =>"judul-post-kedua",
+            "author" =>"Nama author Post 2",
+            "body" =>"dajsfjdas;fjdas;fjdsafjd;safjdsajf;dsjfasj"
+        ],
+    ];
+
+
+    return view('posts',[
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
 });
+
+//halaman single post
+Route::get('posts/{slug}',function ($slug) { //{slug} = wildcard,untuk ngambil isi apapun dari slash nya
+    $blog_posts =[
+        [
+            "title" =>"Judul Post",
+            "slug" =>"judul-post-pertama",
+            "author" =>"Nama author Post",
+            "body" =>"dajsfjdas;fjdas;fjdsafjd;safjdsajf;dsjfasj"
+        ],
+        [
+            "title" =>"Judul Post kedua",
+            "slug" =>"judul-post-kedua",
+            "author" =>"Nama author Post 2",
+            "body" =>"dajsfjdas;fjdas;fjdsafjd;safjdsajf;dsjfasj"
+        ],
+];
+
+    $new_post = [];
+    foreach ($blog_posts as $post)
+    {
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+    return view('post',[
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
+}); 
 
 
